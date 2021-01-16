041c3434c504fc4771519da1cb02fde47f62a374
lamzhihao <70885370+lamzhihao@users.noreply.github.com>
2021-01-16 12:46:36 +0800

    16 Jan 2021
    Still not linked to account page yet

---
+++ CoreApp/CoreApp.Web/ClientApp/src/views/adminSummary/AdminSummary/AdminSummary.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { Pie } from 'react-chartjs-2';
+import {  ActionButton  } from 'src/components/Buttons';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  useTheme,
+  makeStyles,
+  colors
+} from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
+
+const AdminSummary = ({ className, ...rest }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const data = {
+    datasets: [{
+      data: [10, 20, 30],
+      backgroundColor: [
+        colors.green[500],
+        colors.orange[600],
+        colors.red[600],
+      ],
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+      'Active',
+      'Pending',
+      'Suspended'
+    ]
+  };
+
+
+  const options = {
+    layout: { padding: 0 },
+    maintainAspectRatio: false,
+    responsive: true,
+  };
+
+  return (
+     <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardHeader
+        title="Account Summary"
+      />
+      <Divider />
+      <CardContent>
+        <Box
+          height={400}
+          position="relative"
+        >
+          <Pie
+            data={data}
+            options={options}
+          />
+        </Box>
+      </CardContent>
+      <Divider />
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        p={2}
+      >
+        <ActionButton variant="contained" size="lg" color="#ff5c5c" className={classes.exportButton}>
+          rescan
+        </ActionButton>
+
+
+      </Box>
+    </Card>
+       
+  );
+};
+
+AdminSummary.propTypes = {
+  className: PropTypes.string
+};
+
+export default AdminSummary;
+
---
+++ CoreApp/CoreApp.Web/ClientApp/src/views/adminSummary/AdminSummary/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import
+{
+  Container,
+  Grid,
+  makeStyles,
+  Box,
+  Button,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Page from 'src/components/Page';
+import AdminSummary from './AdminSummary';
+import BreadCrumb from './BreadCrumb';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
+const Dashboard = () => {
+  const classes = useStyles();
+
+  return (
+    <Page
+      className={classes.root}
+      title="Admin Summary"
+    >
+      <Container maxWidth={false}>
+        <BreadCrumb />
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          mb={3}
+        >
+        </Box>
+        <Grid
+          container
+        >
+          <Grid item sm={12}>
+            <AdminSummary />
+          </Grid>
+
+          <Grid item sm={12} />
+        </Grid>
+
+
+
+      </Container>
+    </Page>
+  );
+};
+
+export default Dashboard;
