85cb244b952ff495475a253dffd7bba4a94b193d
Daren <darenpuan@gmail.com>
2021-01-20 16:47:34 +0800

    RadioGroup.js css changes

--- CoreApp/CoreApp.Web/ClientApp/src/components/controls/RadioGroup.js
+++ CoreApp/CoreApp.Web/ClientApp/src/components/controls/RadioGroup.js
@@ -1,6 +1,66 @@
 import React from 'react'
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 import { FormControl, FormLabel, RadioGroup as MuiRadioGroup, FormControlLabel, Radio } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  root: {
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+  radioWrapper: {
+    marginBottom: "3%",
+    marginLeft: "2%"
+  },
+  icon: {
+    borderRadius: '50%',
+    width: 16,
+    height: 16,
+    boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+    backgroundColor: '#f5f8fa',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    '$root.Mui-focusVisible &': {
+      outline: '2px auto rgba(19,124,189,.6)',
+      outlineOffset: 2,
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#ebf1f5',
+    },
+    'input:disabled ~ &': {
+      boxShadow: 'none',
+      background: 'rgba(206,217,224,.5)',
+    },
+  },
+  checkedIcon: {
+    backgroundColor: '#137cbd',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+    '&:before': {
+      display: 'block',
+      width: 16,
+      height: 16,
+      backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
+      content: '""',
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#106ba3',
+    },
+  },
+});
+function StyledRadio(props) {
+  const classes = useStyles();
+
+  return (
+    <Radio
+      className={classes.root}
+      disableRipple
+      color="default"
+      checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+      icon={<span className={classes.icon} />}
+      {...props}
+    />
+  );
+}
 export default function RadioGroup(props) {
 
   const { name, label, value, onChange, items } = props;
@@ -15,7 +75,7 @@ export default function RadioGroup(props) {
         {
           items.map(
             item => (
-              <FormControlLabel key={item.id} value={item.id} control={<Radio />} label={item.title} />
+              <FormControlLabel key={item.id} value={item.id} control={<StyledRadio />} label={item.title} />
             )
           )
         }
